9cbe808f920be002b8cbb87acfcd581616320248
giri929 <reddygirish929@gmail.com>
2018-02-11 00:20:29 +0530

    connecting levels

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -6,9 +6,11 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name){
 		Debug.Log("Load Level requested for : " + name);
+		Application.LoadLevel (name);
 	}
 
 	public void QuitRequest(){
 		Debug.Log ("Quit Requested ");
+		Application.Quit ();
 	}
 }
