9b1c09ea0f0479aaa1d66711dd86cbac859536a8
giri929 <reddygirish929@gmail.com>
2018-02-11 22:50:33 +0530

    End of number Wizard with UI

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	int max = 1000;
+	int min = 1;
+	int guess;
+	public int maxcount=10;
+
+	public Text text;
+
+	void Start () {
+		StartGame ();
+		}
+	void StartGame (){
+		max = 1000;
+		min = 1;
+		guess = Random.Range(min,max+1) ;
+		text.text = guess.ToString ();
+
+	}
+
+
+	public void GuessHigher(){
+		min = guess;
+		NextGuess ();
+	}
+
+	public void GuessLower(){
+		max = guess;
+		NextGuess ();
+	}
+
+	void NextGuess(){
+		guess = Random.Range( min , max + 1 );
+		text.text = guess.ToString ();
+		maxcount = maxcount - 1;
+		if (maxcount <= 0) {
+			Application.LoadLevel ("Win");
+		}
+	}
+}
